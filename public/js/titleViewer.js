a4f43ffdbc55fac968f1900f72b157ba5d64eaae
canon <eirinlove@gmail.com>
2022-01-03 17:11:58 +0900

    json, 스레드 이름 바인딩 추가

---
+++ public/js/titleViewer.js
@@ -0,0 +1,28 @@
+const data = require('./threadlist.json'); //json 파일 로딩
+
+exports.renderFunc = 
+
+       /*function titleViewer(thread_id){
+       
+        var titlename = "ㅇㅇㅇ";
+       database.collection('thread_list').findOne({thread_id : parseInt(thread_id)}), function(err, context){ 
+   
+
+
+        titlename = "ㄹㄹㄹ";
+        
+       
+        
+       }
+         };*/
+
+  
+       function titleViewer(thread_id){
+
+        return thread_id;
+
+       }
+
+       
+  
+     
