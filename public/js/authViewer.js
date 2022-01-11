c1ce75031058011a70d67f31bcf43a25235ab843
canon <eirinlove@gmail.com>
2022-01-11 16:07:45 +0900

    ,

--- public/js/authViewer.js
+++ public/js/authViewer.js
@@ -1,9 +1,12 @@
-const data = require('../../keyimpress.json');
+const authdata = require('../../keyimpress.json');
+
 
 exports.authHive = 
 
 function authViewer(){
-        
-        return data[i].keys;
+   
+        return authdata.table[0].keys;
+    
+       // return "안녕하세요";
     }
 
