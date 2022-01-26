9a1fba39cdd206b36f7030faa7bbe93934720ca4
canon <eirinlove@gmail.com>
2022-01-26 22:58:02 +0900

    대댓글 조회 콘솔.

--- routes/comment.js
+++ routes/comment.js
@@ -9,7 +9,26 @@ MongoClient.connect(process.env.DB_URL, function(err, client){
 });
 
 
+router.post('/getComment', function(req,res){
 
+    console.log('쿼리');
+    database.collection('comment').find({post_id : parseInt(req.body.postId)} && {connection_id:{$eq:0}}).toArray(function(err, context){ // 부모가 없는 최상위 댓글
+       
+        database.collection('comment').find({post_id : parseInt(req.body.postId)} && {connection_id: {$gt:0}}).toArray(function(err,context_2){ // 부모가 있는 댓글
+
+//console.log('포스트 id'+req.body.postId+'에 대한 쿼리 결과' + context[0]);
+    //console.log('포스트 id'+req.body.postId+'의 깊이' + context_2[0].depth);
+
+            res.send({PList : context, CList : context_2});
+
+        
+        })
+        
+
+    })
+    
+
+});
 
 
 module.exports = router;
